a2916120aa51d41ac1be166a0e3843ab0d502d1b
David Days <david.days@maplarge.com>
2024-07-05 15:32:01 -0400

    Update test file paths

--- tests/test_lednicer.py
+++ tests/test_lednicer.py
@@ -2,7 +2,7 @@ import wingwalker.lednicer as lednicer
 
 
 def test_lednicer_parsing():
-    parser = lednicer.Parser('lednicerdatfile.txt')
+    parser = lednicer.Parser('tests/lednicerdatfile.txt')
     xs, ys, found_airfoil = parser.read(1.0)
     assert found_airfoil == 'NASA SC(2)-1010 AIRFOIL'
     assert len(xs) == 206
